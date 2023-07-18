93f0d7ab23549120235fa88d3e7481a5db249abf
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-07-19 01:38:54 +0530

    tailwind set up and working

---
+++ components/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-red-600 text-lg">Open up App.js to start working on your app!</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
